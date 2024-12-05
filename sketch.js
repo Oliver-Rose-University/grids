73ae9e16bf1df327412ad6e03c15c63f990a0ce9
Oliver Rose <oliver.rose@student.norwichuni.ac.uk>
2024-12-05 11:43:34 +0000

    genuinely cool

--- sketch.js
+++ sketch.js
@@ -9,31 +9,32 @@ function setup() {
 
 function draw() {
   background(220);
-  const CELLSIZE = 10
-  // noStroke();
-  // Simple Grid
-  // for(let y = 0; y < height; y+= CELLSIZE){
-  //   for(let x = 0; x < width; x+= CELLSIZE){
-  //     fill(random(255), random(255), random(255));
-  //     rect(x, y, CELLSIZE)
-  //   }
-  // }
+  const ARCVALUES = [TAU * 0.25, TAU * 0.5, TAU * 0.75, TAU];
+  const CELLSIZE = 50;
+  for(let y = 0; y < height; y+= CELLSIZE){
+    for(let x = 0; x < width; x+= CELLSIZE){
+      fill(random(255), random(255), random(255));
+      arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE, CELLSIZE, 0, random(ARCVALUES))
+      arc(x + CELLSIZE / 2, y + CELLSIZE / 2, CELLSIZE / 2, CELLSIZE / 2, 0, random(ARCVALUES))
 
-    let x = 0
-    let y = 0
-    let currentWidth = random(CELLSIZES);
-    let currentHeight = random(CELLSIZES);
-    while(y < height){
-      while(x < width){
-        fill(random(COLOURS));
-        rect(x, y, currentWidth, currentHeight);
-        x+= currentWidth;
-        currentWidth = random(CELLSIZES);
-      }
-      x = 0;
-      y += currentHeight;
-      currentHeight = random(CELLSIZES);
+    }
   }
+
+    // let x = 0
+    // let y = 0
+    // let currentWidth = random(CELLSIZES);
+    // let currentHeight = random(CELLSIZES);
+    // while(y < height){
+    //   while(x < width){
+    //     fill(random(COLOURS));
+    //     rect(x, y, currentWidth, currentHeight);
+    //     x+= currentWidth;
+    //     currentWidth = random(CELLSIZES);
+    //   }
+    //   x = 0;
+    //   y += currentHeight;
+    //   currentHeight = random(CELLSIZES);
+  // }
 }
 
 
